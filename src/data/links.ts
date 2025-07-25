d6e0f71b8648a352bdc6c507bef5698f8989a20b
Manohar Solanki <119101769+solankiboy939@users.noreply.github.com>
2025-07-26 00:40:52 +0530

    Update links.ts

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,7 +2,7 @@ export const Links = {
     github: "https://github.com/solankiboy939",
     linkedin: "https://www.linkedin.com/in/mr-manohar-solanki/",
     email: "mailto:solankimanohar2176@gmail.com",
-    resume: "https://drive.google.com/file/d/1fly7lANB1jx63nx-vIpzYNWyH4m22NIq/view?usp=sharing",
+    resume: "https://drive.google.com/file/d/1cjF9zGwlCtbeOVSpXBrcugmEMpWSDhV0/view?usp=sharing",
     emailonly: "solankimanohar2176@gmail.com",
     location: "Phagwara, Punjab",
     phone: "+91 9352512176",
