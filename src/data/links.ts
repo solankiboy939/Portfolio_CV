3c3588b00241665914ffdd4d20932670363a903c
Manohar Solanki <119101769+solankiboy939@users.noreply.github.com>
2026-02-05 02:59:00 +0530

    Update links.ts

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,9 +1,9 @@
 export const Links = {
     github: "https://github.com/solankiboy939",
     linkedin: "https://www.linkedin.com/in/mr-manohar-solanki/",
-    email: "mailto:solankimanohar2176@gmail.com",
-    resume: "https://drive.google.com/file/d/1lyYILP3bKF7_TLej5scRKb2IhlxqW1lA/view?usp=sharing",
-    emailonly: "solankimanohar2176@gmail.com",
+    email: "mailto:Manohar.kumar.cs@gmail.com",
+    resume: "https://drive.google.com/file/d/1dZjgYcCX4Zo3s5HB9VQzKU8ArTlHwQvT/view?usp=sharing",
+    emailonly: "Manohar.kumar.cs@gmail.com",
     location: "Phagwara, Punjab",
     phone: "+91 9352512176",
   };
